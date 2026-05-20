4f0d107c6dbb09e8e6eed2dda3c141b24da7c431
mabi0110 <mariadb0110gmail.com>
2026-05-20 11:58:33 +0200

    initial commit

---
+++ src/test/java/pl/javastart/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
